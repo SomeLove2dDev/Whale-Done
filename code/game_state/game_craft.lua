942677465c8497fb7f52b2c1644cd2f45e618013
SomeLove2dDev <mat.hojun.seong@gmail.com>
2026-01-13 21:03:54 -0800

    More small changes to crafting menu

--- code/game_state/game_craft.lua
+++ code/game_state/game_craft.lua
@@ -4,15 +4,38 @@ game_craft.__index = game_craft
 local scale = require("code.addon.scale")
 local Scale1 = scale:new(768, 432)
 button = require("code.addon.button")
+blocksMain = love.graphics.newImage("assets/blocks.png")
+blocks = {}
+for i=1, 5 do
+    for j=1, 5 do
+        table.insert(blocks, love.graphics.newQuad((i-1) * 16, (j-1) * 16, 16, 16, blocksMain))
+    end
+end
 Buttons = {}
+text = {
+    "bridge",
+    "workbench",
+    "smelter",
+    "item",
+    "item",
+    "item"
+}
+logo = {
+    blocks[1],
+    nil,
+    nil,
+    nil,
+    nil,
+    nil,
+}
 rgb = 1/255
 bqt = 6
 for i=1, bqt do
-    table.insert(Buttons, button:new(400, 30 + (i-1) * 60, 300, 50, "item", 0, 0, {125 * rgb, 125 * rgb, 125 * rgb, 1}, nil))
+    table.insert(Buttons, button:new(400, 30 + (i-1) * 60, 300, 50, text[i], 0, 0, {125 * rgb, 125 * rgb, 125 * rgb, 1}, nil))
 end
 
 Large = {
-    image = button:new(50, 30, 300, 170, "item", 0, 0, {150 * rgb, 150 * rgb, 150 * rgb, 1}, nil),
+    image = button:new(80, 70, 300, 270 ,"item", 0, 0, {150 * rgb, 150 * rgb, 150 * rgb, 1}, nil),
     focus = 1
 }
 
@@ -40,8 +63,10 @@ function game_craft:draw()
     Scale1:draw1()
 
     for _, Button in ipairs(Buttons) do
-        Button:draw()
+        Button:draw(blocksMain)
+
     end
+
     Large.image:draw()
 
     Scale1:draw2()
